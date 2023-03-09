de80b0272a9685506fc369df0d05725017df0a25
alexane-trubert <trubert.alexane@orange.fr>
2023-03-09 12:01:06 +0100

    [WIP] formulaire de recherche

--- js/api.js
+++ js/api.js
@@ -1,18 +1,23 @@
-function getAllJobs(offset, onSucess, onError, loadMoreBtn) {
+function getAllJobs(offset, onSucess, onError, loadMoreBtn, text, jobLocation, fulltime, limit) {
+    if(fulltime.checked === true) {
+        fulltime = 1;
+    }else {
+        fulltime = 0;
+    }
     const request = new XMLHttpRequest();
-    request.open("GET", "https://ecf-dwwm.cefim-formation.org/api/jobs?offset=" + offset);
+    request.open("GET",
+        "https://ecf-dwwm.cefim-formation.org/api/jobs/search?offset=" + offset + "&text=" + text + "&location=" + jobLocation + "&fulltime=" + fulltime + "&limit=" + limit);
     request.addEventListener("readystatechange", function () {
-        if(request.readyState === XMLHttpRequest.DONE) {
+        if (request.readyState === XMLHttpRequest.DONE) {
             loadMoreBtn.disabled = false;
-            if(request.status === 200) {
+            if (request.status === 200) {
                 const response = JSON.parse(request.responseText);
                 onSucess(response);
-                if(offsetJob >= response.total) {
+                if (offsetJob >= response.total) {
                     loadMoreBtn.textContent = "No more data";
                     loadMoreBtn.className = "btn btn-load btn-disabled";
                     loadMoreBtn.disabled = true;
-                }else {
-                    // console.log("not mouh");
+                } else {
                     loadMoreBtn.textContent = "Load More";
                 }
             } else {
@@ -29,8 +34,8 @@ function getOneJobs(id, onSucess, onError) {
     const request = new XMLHttpRequest();
     request.open("GET", "https://ecf-dwwm.cefim-formation.org/api/job/" + id);
     request.addEventListener("readystatechange", function () {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            if(request.status === 200) {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
                 const response = JSON.parse(request.responseText);
                 onSucess(response);
             } else {
--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 const inputFormTitle = document.querySelector(".filter-title");
 const loadMoreBtn = document.querySelector(".btn-load");
 const overlay = document.querySelector(".overlay");
+let offsetJob = 0;
+const mainContent = document.querySelector(".cards-list");
+const searchBtn = document.querySelector(".btn-modal");
 
 // Ouverture modale au clic sur le bouton filtre
 const btnFilter = document.querySelector(".filter");
 const modal = document.querySelector(".form-group-modal");
+
+// Moteur de recherche
+const form = document.querySelector(".main-form");
+const text = document.querySelector(".filter-title").value;
+const jobLocation = document.querySelector("#location").value;
+const fulltime = document.querySelector(".checkbox");
+const limit = 12;
 
 //modification du placeholder en fonction de la taille de l'écran
 function placeholder() {
@@ -25,7 +35,6 @@ btnFilter.addEventListener("click", function() {
 placeholder();
 window.addEventListener("resize", placeholder);
 
-let offsetJob = 0;
 function getMoreJobs() {
     getAllJobs(
         offsetJob,
@@ -38,8 +47,40 @@ function getMoreJobs() {
         function () {
             alert("Erreur !");
         },
-        loadMoreBtn
+        loadMoreBtn,
+        text,
+        jobLocation,
+        fulltime,
+        limit
     );
 }
+
 getMoreJobs();
 loadMoreBtn.addEventListener("click", getMoreJobs);
+
+form.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    mainContent.innerHTML = "";
+    offsetJob = 0;
+
+    const text = document.querySelector(".filter-title").value;
+    const jobLocation = document.querySelector("#location").value;
+    const fulltime = document.querySelector(".checkbox");
+
+    getAllJobs(
+        0,
+        function(data) {
+            data.jobs.forEach(jobs => {
+                addJobs(jobs.company, jobs.contract, jobs.id, jobs.location, jobs.logo, jobs.logoBackground, jobs.position, jobs.postedAt);
+            });
+        },
+        function() {
+            alert("Erreur !");
+        },
+        loadMoreBtn,
+        text,
+        jobLocation,
+        fulltime,
+        limit
+    );
+});
